a8dd29197c99bf3fb5bd3a43cc3a2aae4c75e2fb
Daneil <daneilgreaves@gmail.com>
2021-02-20 13:15:57 -0500

    Add cal feature

--- app/model.js
+++ app/model.js
@@ -2,23 +2,23 @@ import { action, thunk } from 'easy-peasy';
 import giftCardsApi from './api/giftcards';
 
 export default {
-  //States
-  giftCards: [],
-  giftCard: {},
+	//States
+	giftCards: [],
+	giftCard: {},
 
-  //Thunks
-  fetchGiftCards: thunk(async (actions, payload) => {
-    const response = await giftCardsApi.getGiftCards();
-    actions.setGiftCards(response);
-  }),
+	//Thunks
+	fetchGiftCards: thunk(async (actions, payload) => {
+		const response = await giftCardsApi.getGiftCards();
+		actions.setGiftCards(response);
+	}),
 
-  //Actions
-  setGiftCards: action((state, giftCards) => {
-    state.giftCards = giftCards;
-  }),
+	//Actions
+	setGiftCards: action((state, giftCards) => {
+		state.giftCards = giftCards;
+	}),
 
-  setGiftCard: action((state, giftCard) => {
-    console.log('id in stat', giftCard);
-    state.giftCard = giftCard;
-  }),
+	setGiftCard: action((state, giftCard) => {
+		console.log('id in state', giftCard);
+		state.giftCard = giftCard;
+	}),
 };
