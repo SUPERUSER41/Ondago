608a04d4d2cfa371f50aff9f5b21c243a5dd4652
Daneil <daneilgreaves@gmail.com>
2021-02-28 13:21:44 -0500

    make switch tabs

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import AppHeader from '../components/AppHeader';
 import FAB from '../components/FAB';
 import AppTabNavigator from '../navigation/AppTabNavigator';
 
+const SCREENS = ['Marketplace', 'EGiftCards'];
+
+const TABS = {
+	nearby: 'Nearby ',
+	online: 'Online ',
+	available: 'Available ',
+	redeemed: 'Redeemed ',
+};
+
 const MainScreen = () => {
+	const [currentScreen, setCurrentScreen] = useState(SCREENS[0]);
+
 	return (
 		<View style={styles.container}>
 			<AppHeader />
-			<AppTabNavigator />
+			{currentScreen === SCREENS[0] && (
+				<AppTabNavigator tab1Name={TABS.nearby} tab2Name={TABS.online} />
+			)}
+			{currentScreen === SCREENS[1] && (
+				<AppTabNavigator tab1Name={TABS.available} tab2Name={TABS.redeemed} />
+			)}
 			<View style={styles.buttonContainer}>
-				<FAB iconName="store" title="Marketplace" isActive={true} />
-				<FAB iconName="card-giftcard" title="My eGift Cards" isActive={false} />
+				<FAB
+					onPress={() => setCurrentScreen(SCREENS[0])}
+					iconName="store"
+					title="Marketplace"
+					isActive={currentScreen === SCREENS[0]}
+				/>
+				<FAB
+					onPress={() => setCurrentScreen(SCREENS[1])}
+					iconName="card-giftcard"
+					title="My eGift Cards"
+					isActive={currentScreen === SCREENS[1]}
+				/>
 			</View>
 		</View>
 	);
